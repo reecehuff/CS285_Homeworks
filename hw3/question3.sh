2bf5b5076aad75a4f99a604845623fb96430098a
reecehuff <reecedhuff@gmail.com>
2022-10-11 00:56:22 -0700

    Added bash script for question 3

---
+++ hw3/question3.sh
@@ -0,0 +1,4 @@
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_hparam1 --lunar_lr 1e-4
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_hparam2 --lunar_lr 1e-3
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_hparam3 --lunar_lr 1e-2
+python cs285/scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_hparam4 --lunar_lr 3e-2
