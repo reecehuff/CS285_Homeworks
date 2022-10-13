d253941956e97883bef67a2837ff3f9151f2d335
reecehuff <reecedhuff@gmail.com>
2022-10-13 13:05:36 -0700

    Added bash script for question 6

---
+++ hw3/question6.sh
@@ -0,0 +1,1 @@
+python cs285/scripts/run_hw3_sac.py --env_name InvertedPendulum-v4 --ep_len 1000 --discount 0.99 --scalar_log_freq 1000 -n 100000 -l 2 -s 10 -b 1000 -eb 2000 -lr 0.0003 --init_temperature 0.1 --exp_name q6a_sac_InvertedPendulum --seed 1
