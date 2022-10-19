5bb367d7a1b669e9137970758368e9874f1b7965
reecehuff <reecedhuff@gmail.com>
2022-10-18 20:13:21 -0700

    Updated question 6 bash script
    Added command for cheetah

--- hw3/question6.sh
+++ hw3/question6.sh
@@ -1,1 +1,3 @@
-python cs285/scripts/run_hw3_sac.py --env_name InvertedPendulum-v4 --ep_len 1000 --discount 0.99 --scalar_log_freq 1000 -n 100000 -l 2 -s 256 -b 1000 -eb 2000 -lr 0.0003 --init_temperature 0.1 --exp_name q6a_sac_InvertedPendulum --seed 9 --actor_update_frequency 10
+# python cs285/scripts/run_hw3_sac.py --env_name InvertedPendulum-v4 --ep_len 1000 --discount 0.99 --scalar_log_freq 1000 -n 100000 -l 2 -s 256 -b 1000 -eb 2000 -lr 0.0003 --init_temperature 0.1 --exp_name q6a_sac_InvertedPendulum --seed 9 --actor_update_frequency 10
+
+python cs285/scripts/run_hw3_sac.py --env_name HalfCheetah-v4 --ep_len 150 --discount 0.99 --scalar_log_freq 1500 -n 2000000 -l 2 -s 256 -b 512 -eb 1500 -lr 0.00001 --init_temperature 0.1 --exp_name q6b_sac_HalfCheetah --seed 12 --actor_update_frequency 10 
