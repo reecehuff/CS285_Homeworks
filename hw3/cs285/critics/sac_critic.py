4703e88526cc06fdd84c0b8db03cb05d39cff4aa
reecehuff <reecedhuff@gmail.com>
2022-10-18 23:09:50 -0700

    Implemented correct critic

--- hw3/cs285/critics/sac_critic.py
+++ hw3/cs285/critics/sac_critic.py
@@ -54,4 +54,10 @@ class SACCritic(nn.Module, BaseCritic):
 
     def forward(self, obs: torch.Tensor, action: torch.Tensor):
         # TODO: return the two q values
+
+        obs_action = torch.cat([obs, action], dim=-1)
+        q1 = self.Q1(obs_action)
+        q2 = self.Q2(obs_action)
+        values = tuple((q1,q2))
+
         return values
