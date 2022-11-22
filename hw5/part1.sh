373cbdd16af1652d790d081cb57ebcf4438d4add
reecehuff <reecedhuff@gmail.com>
2022-11-21 22:14:02 -0800

    Renamed part 1 bash script

--- hw5/part1.sh
+++
@@ -1,7 +0,0 @@
-python cs285/scripts/run_hw5_expl.py --env_name PointmassEasy-v0 --use_rnd --unsupervised_exploration --exp_name q1_env1_rnd
-
-# python cs285/scripts/run_hw5_expl.py --env_name PointmassEasy-v0 --unsupervised_exploration --exp_name q1_env1_random
-
-# python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --use_rnd --unsupervised_exploration --exp_name q1_env2_rnd
-
-# python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --unsupervised_exploration --exp_name q1_env2_random
