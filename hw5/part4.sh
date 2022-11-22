2a9300b2a6334f379566c4d5505debb1c7d525d4
reecehuff <reecedhuff@gmail.com>
2022-11-21 22:13:29 -0800

    Part 4 bash script

---
+++ hw5/part4.sh
@@ -0,0 +1,10 @@
+
+for lambda in 0.1 1 2 10 20 50
+do
+
+    python cs285/scripts/run_hw5_awac.py --env_name PointmassEasy-v0 --exp_name q4_awac_easy_unsupervised_lam$lambda --use_rnd --num_exploration_steps=20000 --unsupervised_exploration --awac_lambda=$lambda
+
+    python cs285/scripts/run_hw5_awac.py --env_name PointmassEasy-v0 --use_rnd --num_exploration_steps=20000 --awac_lambda=$lambda --exp_name q4_awac_easy_supervised_lam$lambda
+
+done
+
