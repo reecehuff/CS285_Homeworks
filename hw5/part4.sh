2123cc6be1db42cf2ea032da74dd7eef5b0c7e11
reecehuff <reecedhuff@gmail.com>
2022-11-22 15:02:34 -0800

    Finished part 4 bash script

--- hw5/part4.sh
+++ hw5/part4.sh
@@ -1,4 +1,3 @@
-
 for lambda in 0.1 1 2 10 20 50
 do
 
@@ -8,3 +7,11 @@ do
 
 done
 
+for lambda in 0.1 1 2 10 20 50
+do
+
+    python cs285/scripts/run_hw5_awac.py --env_name PointmassMedium-v0 --exp_name q4_awac_medium_unsupervised_lam$lambda --use_rnd --num_exploration_steps=20000 --unsupervised_exploration --awac_lambda=$lambda 
+    
+    python cs285/scripts/run_hw5_awac.py --env_name PointmassMedium-v0 --use_rnd --num_exploration_steps=20000 --awac_lambda=$lambda --exp_name q4_awac_medium_supervised_lam$lambda 
+
+done
