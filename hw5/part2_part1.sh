fd96f7c0182901b9fde87298f2e7c7293beb1cca
reecehuff <reecedhuff@gmail.com>
2022-11-24 12:04:32 -0800

    Added shift + scale

--- hw5/part2_part1.sh
+++ hw5/part2_part1.sh
@@ -3,3 +3,5 @@
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --exp_name q2_dqn --use_rnd --unsupervised_exploration --offline_exploitation --cql_alpha=0
 
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --exp_name q2_cql --use_rnd --unsupervised_exploration --offline_exploitation --cql_alpha=0.1
+
+python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --exp_name q2_cql_shift_scale --use_rnd --unsupervised_exploration --offline_exploitation --cql_alpha=0.1 --exploit_rew_shift=1 --exploit_rew_scale=100
